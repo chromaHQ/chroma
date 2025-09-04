006aa4a1108bb52b6f6016aac0ac35e0aa5dfd1c
Jefferson <jeffersonlicet@gmail.com>
2025-09-04 17:27:26 -0300

    fix: Exporting container

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -20,6 +20,7 @@ export * from './scheduler/decorators/Every';
 
 export { Booteable, isBooteable, isDestroyable } from './services/booteable';
 
+export { container } from './di/Container';
 export * from './scheduler/core/IJob';
 export * from './scheduler/core/Job';
 export * from './scheduler/decorators/Delay';
