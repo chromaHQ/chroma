eecc00655297c56fe82d258a838a4b2b2a07279b
Jefferson <jeffersonlicet@gmail.com>
2025-12-05 15:37:39 -0300

    feat: move service worker health exports to index.tsx

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -4,3 +4,14 @@ export { useBridgeQuery } from './hooks/useBridgeQuery';
 export { useBridgeMutation } from './hooks/useBridgeMutation';
 export { useConnectionStatus } from './hooks/useConnectionStatus';
 export type { ConnectionStatusResult } from './hooks/useConnectionStatus';
+
+// Service Worker Health - Single source of truth for SW health status
+export {
+  ServiceWorkerHealthProvider,
+  useServiceWorkerHealth,
+  useServiceWorkerHealthSimple,
+  subscribeToHealth,
+  getHealthStatus,
+  type HealthStatus,
+  type ServiceWorkerHealthContextValue,
+} from './ServiceWorkerHealth';
