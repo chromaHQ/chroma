fed9e7bc8d65f1cd63d46cd4424482a8d2093a6e
Jefferson <jeffersonlicet@gmail.com>
2025-12-05 15:39:49 -0300

    feat: add index.ts for service worker health exports

---
+++ packages/react/src/index.ts
@@ -0,0 +1,15 @@
+export { BridgeProvider } from './BridgeProvider';
+export { useBridge } from './hooks/useBridge';
+export { useBridgeQuery } from './hooks/useBridgeQuery';
+export { useConnectionStatus } from './hooks/useConnectionStatus';
+
+// Service Worker Health - Single source of truth for SW health status
+export {
+  ServiceWorkerHealthProvider,
+  useServiceWorkerHealth,
+  useServiceWorkerHealthSimple,
+  subscribeToHealth,
+  getHealthStatus,
+  type HealthStatus,
+  type ServiceWorkerHealthContextValue,
+} from './ServiceWorkerHealth';
